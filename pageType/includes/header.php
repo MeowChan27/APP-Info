4bdd33c886d3fccd1599088aa57a85364a713b7b
Selrahc <char.mailley@gmail.com>
2023-03-05 09:24:41 +0100

    split pagePage

---
+++ pageType/includes/header.php
@@ -0,0 +1,29 @@
+<!--******************************************************
+* APP - Projet Système Numérique - Composante Informatique
+* ISEP - A1 - G7C
+* Semestre 2
+* Auteur : - MAILLEY_Charles 
+           - MAIZA_Fares
+           - MARTINEZ_Eliot
+           - PAVIOT-ADET_Flore
+           - SPASOJEVIC_Fanny
+           - VINGADASSAMY_Prasanaa
+* Date de rendu  : 05/06/2023
+********************************************************-->
+
+<html>
+    <body>
+
+        <header class="header_all">
+            <a href="main.html"><img class="header_logo" src="img/Logo_Appea-grey.svg" alt="Logo APPNEA">APPNEA</a>
+            <ul>
+                <li><a href="">Nos produits</a></li>
+                <li><a href="">Les capteurs</a></li>
+                <li><a href="">Notre équipe</a></li>
+                <li><a href="">FAQ</a></li>
+                <li class="header_log"><a href= "">Connexion</a></li>
+            </ul>
+        </header>
+        
+    </body>
+</html>
---
+++ pageType/mainType.php
@@ -0,0 +1,48 @@
+<!--******************************************************
+* APP - Projet Système Numérique - Composante Informatique
+* ISEP - A1 - G7C
+* Semestre 2
+* Auteur : - MAILLEY_Charles 
+           - MAIZA_Fares
+           - MARTINEZ_Eliot
+           - PAVIOT-ADET_Flore
+           - SPASOJEVIC_Fanny
+           - VINGADASSAMY_Prasanaa
+* Date de rendu  : 05/06/2023
+********************************************************-->
+
+<!DOCTYPE html>
+<html lang="fr">
+
+    <head>
+        <meta charset="UTF-8">
+        <title>APNEA</title>
+        <link rel="icon" type="image/x-icon" href="img/Logo_Appea-color.svg">
+        <link rel="stylesheet" href="css/mainType.css" />
+        <link href="https://fonts.googleapis.com/css2?family=Poppins:wght@400;500;600;700&display=swap" rel="stylesheet">
+        <link href="boxicons/css/boxicons.min.css" rel="stylesheet">
+    </head>
+
+    <body>
+        <?php include 'includes/header.php'; ?>
+
+    
+        <main class="main_all">
+        <!-- L'ensemble du contenu de votre page est à placer ici-->
+        <!-- ....................................................-->
+        <!-- ....................................................-->
+        <!-- ....................................................-->
+        <!-- ....................................................-->
+        <!-- ....................................................-->
+        <!-- ....................................................-->
+        <!-- ....................................................-->
+        <!-- ....................................................-->
+        </main>
+
+
+        <?php include 'includes/footer.php'; ?>
+    </body>
+</html>
+
+
+
